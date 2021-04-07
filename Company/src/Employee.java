02fe4a02a654d56e2f1799ce55e2e9aeb6e1f49d
sysadmin <sysadmin@bcalab1-m15.mshome.net>
2021-04-07 12:21:13 +0530

    Method added

--- Company/src/Employee.java
+++ Company/src/Employee.java
@@ -8,6 +8,12 @@ public class Employee
 	this.name = name;
 	this.dept = dept;
 	}
+	
+	void display()
+	{
+	System.out.println("Name is" + name);
+	System.out.println("Dept is" +dept);
+	}
 
 	public static void main(String[] args) 
 	{
